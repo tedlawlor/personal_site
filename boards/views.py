c511ed896343c175f3adeeeca3131f8361a66d60
Edward Lawlor <TedLawlor@Teds-MacBook-Pro.local>
2019-11-12 17:42:37 -0500

    funny test

--- boards/views.py
+++ boards/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Teds Site is underway... Stay tuned!')
+    return HttpResponse('Adam is wearing a UVM hat right now')
